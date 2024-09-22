8848e6b3bda5853d2412209b4d2ec42be9adeda3
vitorpicolotto <picolottovitor@gmail.com>
2024-09-22 19:30:14 -0300

    ✨ feat: Login - inclusão das funcionalidades de login

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,16 @@
 const { Router } = require ('express');
 const usuariosRoutes = require("./usuarios.routes");
+const loginRoutes = require("./login.routes");
 
 
 const routes = new Router();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./doc.swagger.json');
+routes.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 routes.use('/usuario', usuariosRoutes)
-
+routes.use("/login",loginRoutes);
 
 module.exports = routes;
