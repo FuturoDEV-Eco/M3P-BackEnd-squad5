a39a81f65e7f3cb2edf903d9f87a17f8eab7ad6f
vitorpicolotto <picolottovitor@gmail.com>
2024-09-22 19:40:17 -0300

    ✨ feat: Locais - criação das rotas, models, migration e controller

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require ('express');
 const usuariosRoutes = require("./usuarios.routes");
 const loginRoutes = require("./login.routes");
+const localRoutes = require("./locais.routes");
 
 
 const routes = new Router();
@@ -12,5 +13,6 @@ routes.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 routes.use('/usuario', usuariosRoutes)
 routes.use("/login",loginRoutes);
+routes.use("/local",validaToken,localRoutes)
 
 module.exports = routes;
