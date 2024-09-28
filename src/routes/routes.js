0effc01d154cbc337a3f03ce92613f9196b784d8
Matheus Farias <matheusfarias95@hotmail.com>
2024-09-28 18:58:17 -0300

    ✨ feat: controller e rotas do dashboard

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,15 @@
-const {Router} = require('express');
-const usuariosRoutes = require('./usuarios.routes');
-const loginRoutes = require('./login.routes');
-const localRoutes = require('./locais.routes');
-const validaToken = require('../midllewares/validatoken');
+const { Router } = require("express");
+const usuariosRoutes = require("./usuarios.routes");
+const loginRoutes = require("./login.routes");
+const localRoutes = require("./locais.routes");
+const validaToken = require("../midllewares/validatoken");
+const dashboardRoutes = require("./dashboard.routes");
 
 const routes = new Router();
 
-routes.use("/usuarios",usuariosRoutes)
+routes.use("/usuarios", usuariosRoutes);
 routes.use("/login", loginRoutes);
-routes.use("/local",validaToken,localRoutes)
+routes.use("/local", validaToken, localRoutes);
+routes.use("/dashboard", dashboardRoutes);
 
-module.exports = routes
+module.exports = routes;
