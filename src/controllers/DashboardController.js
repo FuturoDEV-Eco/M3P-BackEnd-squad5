822950d1548ebb4e0c45da0ab3b42b88f89d4053
Matheus Farias <matheusfarias95@hotmail.com>
2024-09-30 19:59:05 -0300

    ✨ feat: adicionando rota e função para exibir todos os locais cadastrados.

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -16,6 +16,16 @@ class DashboardController {
         .json({ error: "Erro ao buscar total de locais e usuarios" });
     }
   }
+  async LocaisCadastrados(req, res) {
+    try {
+      const locaisCadastrados = await Locais.findAll();
+      return res.status(200).json(locaisCadastrados);
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: "Erro ao buscar locais cadastrados" });
+    }
+  }
 }
 
 module.exports = new DashboardController();
